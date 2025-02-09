d35176a3f745971e4b8ef71637f2713cabc0b629
ifalfahri <ifaltampan69@gmail.com>
2025-02-09 21:15:34 +0800

    refactor: remove unused React import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Form from './components/Form';
 import FormV2 from './components/FormV2';
